0fa16ce07cb6f5a536a7eb83150e669b7622fb15
AndyVera <andy.vera13@csu.fullerton.edu>
2022-09-06 16:23:43 -0700

    Initial Commit

---
+++ Live Demo - Simple IOS App/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Live Demo - Simple IOS App
+//
+//  Created by Andy Vera on 9/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        //Want to put any code that sets up the view
+        //If you have multiple different views, viewDidLoad isn't going to work properly
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
